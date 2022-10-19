a966a1092ba622ee4e827a9b699039b8d7bff2ce
Abhilash Karnale <55885608+Abhilash249@users.noreply.github.com>
2022-10-19 12:59:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node22!\n'
+  const msg = 'Welocme to NewYork\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec diam scelerisque, lacinia justo vel, elementum enim. Praesent at pharetra justo. Cras quis odio quis diam auctor hendrerit non vel ipsum. Mauris posuere augue nec orci hendrerit vulputate. Proin luctus interdum ante in interdum. Nulla facilisi. Quisque rhoncus magna urna, sed ornare ipsum pharetra nec. Cras convallis vehicula ex, eu sodales nisi consectetur viverra.!\n'
   res.end(msg);
 });
 
